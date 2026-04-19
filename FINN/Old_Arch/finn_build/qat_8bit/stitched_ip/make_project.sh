7ef0d0da4b1db66e5146ee1e7e8b8d2b24e6d090
Emanuel <emanuell.hc@gmail.com>
2026-04-18 22:52:48 -0600

    Adding CNV model working on FINN synthesis and Kria deployment

--- FINN/Old_Arch/finn_build/qat_8bit/stitched_ip/make_project.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash 
-cd /tmp/finn_dev_emanuel-hernandez/vivado_stitch_proj_qtyu16g0
-vivado -mode batch -source make_project.tcl
-cd /home/emanuel-hernandez/finn/notebooks/SR_PROJECT
--- FINN/SR_PROJECT/finn_build_v2/fc/stitched_ip/make_project.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash 
-cd /tmp/finn_dev_emanuel-hernandez/vivado_stitch_proj_k91zzi80
-vivado -mode batch -source make_project.tcl
-cd /home/emanuel-hernandez/finn/notebooks/SR_PROJECT
