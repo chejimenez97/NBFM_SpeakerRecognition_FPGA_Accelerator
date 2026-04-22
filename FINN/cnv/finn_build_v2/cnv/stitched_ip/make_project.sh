9deea9164be688aceef594edb2eafea7443a724c
Emanuel <emanuell.hc@gmail.com>
2026-04-22 14:08:22 -0600

    Added working FINN models for 4-bits and 8-bits, for both FC and CNV architectures

--- FINN/cnv/finn_build_v2/cnv/stitched_ip/make_project.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash 
-cd /tmp/finn_dev_emanuel-hernandez/vivado_stitch_proj_pm9ik9x1
-vivado -mode batch -source make_project.tcl
-cd /home/emanuel-hernandez/finn/notebooks/SR_PROJECT
---
+++ FINN/cnv_synthesis/4-bits/finn_build/cnv/stitched_ip/make_project.sh
@@ -0,0 +1,4 @@
+#!/bin/bash 
+cd /tmp/finn_dev_emanuel-hernandez/vivado_stitch_proj_1tvr0_tu
+vivado -mode batch -source make_project.tcl
+cd /home/emanuel-hernandez/finn/notebooks/SR_PROJECT/cnv_synthesis
